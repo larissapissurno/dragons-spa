b4933ed7455e8989475d7100ac066f4305a84009
Larissa Pissurno <larissapissurno@gmail.com>
2020-07-13 22:21:44 -0300

    Updates

---
+++ precache-manifest.21e95acec7e9f0e5b5bbb5e2aed7735e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4117b3c3276c25fdb0d59c7f5d4a487",
+    "url": "/dragons-spa/index.html"
+  },
+  {
+    "revision": "b5dc90cd9a6f53c76076",
+    "url": "/dragons-spa/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b5dc90cd9a6f53c76076",
+    "url": "/dragons-spa/static/js/2.bf1899bc.chunk.js"
+  },
+  {
+    "revision": "d853757cba56cba855c9bbe84516bf2a",
+    "url": "/dragons-spa/static/js/2.bf1899bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c753f4d2cbaea24f8a03",
+    "url": "/dragons-spa/static/js/main.5ab90f0e.chunk.js"
+  },
+  {
+    "revision": "d75c34b75e88854d5f59",
+    "url": "/dragons-spa/static/js/runtime-main.8d57825d.js"
+  },
+  {
+    "revision": "2cd18528a9b583af10aa56c483f2620b",
+    "url": "/dragons-spa/static/media/dragongrey.2cd18528.png"
+  }
+]);

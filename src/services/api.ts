949a9527a679f528ee66809b14994d87d94f3c17
Larissa Pissurno <larissapissurno@gmail.com>
2020-07-13 23:14:13 -0300

    Adjust on service path

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import UsersService from './UsersService';
 import SessionsService from './SessionsService';
 
 const api = axios.create({
-  baseURL: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/',
+  baseURL: 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/',
 });
 
 export const usersService = new UsersService();
